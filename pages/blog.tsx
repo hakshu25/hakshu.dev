52d37910535ecb8547f66b97b8cd7b985b1bbb7c
hakshu <flyhighup.air25@gmail.com>
2021-09-21 11:39:51 +0900

    style: styling blog posts list

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,30 +1,29 @@
-import { makeStyles } from '@material-ui/core';
+import { List, ListItem, ListItemText, Divider } from '@material-ui/core';
 import { GetStaticProps, NextPage } from 'next';
 import Link from 'next/link';
 import { HeaderBar } from '../components/header-bar'
 import { blogClient, Post } from '../lib/blog-client';
-
-const useStyles = makeStyles(() => ({
-}));
+import { formatDateTime } from '../lib/date-util';
 
 type Props = { posts: Post[] };
 
 const Blog: NextPage<Props> = ({ posts }) => {
-  const styles = useStyles();
-
   return (
     <>
       <HeaderBar />
       <main>
-        <ul>
+        <List>
           {posts.map((post) => (
             <li key={post.id}>
-              <Link href={`/blog/${post.id}`}>
-                <a>{post.title}</a>
+              <Link href={`/blog/${post.id}`} passHref={true}>
+                <ListItem button component="a">
+                  <ListItemText primary={post.title} secondary={formatDateTime(post.publishedAt)} />
+                </ListItem>
               </Link>
+              <Divider />
             </li>
           ))}
-        </ul>
+        </List>
       </main>
     </>
   )
