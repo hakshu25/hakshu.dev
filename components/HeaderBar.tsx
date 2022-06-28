c691004a27221b5420b252a9384ae9b0a554ada3
hakshu <flyhighup.air25@gmail.com>
2022-06-28 13:32:35 +0900

    feat: ヘッダーバーの構成変更

--- components/HeaderBar.tsx
+++ components/HeaderBar.tsx
@@ -1,17 +1,28 @@
-import { AppBar, Button, Toolbar } from '@mui/material';
 import Link from 'next/link';
 
 export const HeaderBar = () => {
   return (
-    <AppBar position="sticky">
-      <Toolbar>
-        <Link href="/" passHref={true}>
-          <Button color="inherit">Top</Button>
-        </Link>
-        <Link href="/blog" passHref={true} prefetch={false}>
-          <Button color="inherit">Blog</Button>
-        </Link>
-      </Toolbar>
-    </AppBar>
+    <header className="bg-midnight h-60 text-white px-4 pt-2 grid grid-cols-12 grid-rows-3">
+      <div className="text-2xl col-start-1 col-span-4">
+        Hakshu&apos;s Portfolio
+      </div>
+      <nav className="col-start-12">
+        <ul className="list-none grid grid-cols-2 place-items-end">
+          <li>
+            <Link href="/" passHref={true}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog" passHref={true} prefetch={false}>
+              Blog
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <h1 className="text-7xl font-bold text-center row-start-2 col-span-12">
+        About
+      </h1>
+    </header>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{tsx}', './components/**/*.{tsx}'],
+  content: ['./pages/**/*.tsx', './components/**/*.tsx'],
   theme: {
     extend: {},
+    colors: {
+      midnight: '#28293E',
+      white: colors.white,
+    },
   },
   plugins: [],
 };
