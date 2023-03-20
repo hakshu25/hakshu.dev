5d4e6e63abf680ab5bba7c0db540bc3b9f336794
hakshu <flyhighup.air25@gmail.com>
2023-03-20 19:15:49 +0900

    style: ウインドウサイズに合わせてトップページの画像の高さを調整

--- src/components/HeaderImage.tsx
+++ src/components/HeaderImage.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export const HeaderImage = () => {
   return (
-    <div className="relative h-96">
+    <div className="relative h-60 sm:h-60 md:h-72 lg:h-80">
       <Image src="/img/header-image.png" alt="Picture of top image" fill />
       <div className="relative h-full text-white text-right pr-10">
         <p className="flex justify-end items-center h-full text-7xl font-bold font-sans">
