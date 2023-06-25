6532af3411d74aa5cbfcfc8b505fcb3cb1e74c2f
hakshu <flyhighup.air25@gmail.com>
2023-06-25 18:45:46 +0900

    test: vitestのテスト対象判定が__tests__ディレクトリ配下すべてに広がったため対象を明示的に指定する

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './__tests__/setup.ts',
+    include: ['./__tests__/**/*.test.{ts,tsx}'],
     coverage: {
       provider: 'c8',
       all: true,
